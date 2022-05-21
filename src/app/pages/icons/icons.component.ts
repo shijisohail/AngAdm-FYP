c61eef549e081c83623d292c5165c723b92457af
Saim <saimzhd4@gmail.com>
2022-05-21 20:53:41 +0500

    Added form to System Details Page

--- src/app/pages/icons/icons.component.ts
+++ src/app/pages/icons/icons.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgbModal, NgbModalConfig} from '@ng-bootstrap/ng-bootstrap';
 import {AccordionModule} from 'primeng/accordion';
-
+import { HttpClient } from '@angular/common/http';  
 
 @Component({
   selector: 'app-icons',
@@ -10,10 +10,22 @@ import {AccordionModule} from 'primeng/accordion';
 })
 export class IconsComponent implements OnInit {
 
-  constructor(config: NgbModalConfig, private modalService: NgbModal) {config.backdrop = 'static';
-                                                                       config.keyboard = false;}
+  constructor(config: NgbModalConfig, private modalService: NgbModal,private http:HttpClient) 
+  { 
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+onSubmit(val){
+  console.log(val);
+}
 
-  ngOnInit() {}
+
+  variable:any;
+  ngOnInit():void {
+    this.variable=this.http.get("http://127.0.0.1:8000/attackinfo").subscribe(
+      data=>this.variable=data
+    );
+  }
 
   changeBtn(id) {
     if (document.getElementById(id).innerText === 'Off') {
