3bd121f2d0ff9cd90799403c7a56116fbb8fe71d
Saim <saimzhd4@gmail.com>
2022-05-23 15:20:36 +0500

    POST request working for system details(icons) page

--- src/app/pages/icons/icons.component.ts
+++ src/app/pages/icons/icons.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {NgbModal, NgbModalConfig} from '@ng-bootstrap/ng-bootstrap';
 import {AccordionModule} from 'primeng/accordion';
 import { HttpClient } from '@angular/common/http';  
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-icons',
@@ -9,14 +10,53 @@ import { HttpClient } from '@angular/common/http';
   providers: [NgbModalConfig, NgbModal]
 })
 export class IconsComponent implements OnInit {
-
-  constructor(config: NgbModalConfig, private modalService: NgbModal,private http:HttpClient) 
+  form: FormGroup;
+  constructor(config: NgbModalConfig, private modalService: NgbModal,private http:HttpClient, public fb:FormBuilder) 
   { 
     config.backdrop = 'static';
     config.keyboard = false;
+    this.form = this.fb.group({
+      AttackTitle: [''],
+      AttackDescription: [null]
+    })
   }
-onSubmit(val){
-  console.log(val);
+  AttackTitle:any;
+  AttackDescription:any;
+onSubmit(){
+  
+var val={
+  AttackTitle:this.AttackTitle,
+  AttackDescription:this.AttackDescription
+};
+return this.http.post('http://127.0.0.1:8000/attackinfo', val).subscribe
+(
+  (response) => {
+  return console.log(response);
+},
+  (error) => console.log(error)
+)
+
+
+  // var formData:any = new FormData();
+  // formData.append("AttackTitle",this.form.get('AttackTitle').value);
+  // formData.append("AttackDescription",this.form.get('AttackDescription').value);
+  
+  // return this.http.post('http://127.0.0.1:8000/attackinfo', JSON.stringify(formData)).subscribe
+  // (
+  //     (response) => {
+  //     return console.log(response);
+  //   },
+  //     (error) => console.log(error)
+  //   )
+
+  // this.AttackTitle=val.AttackTitle;
+  // this.AttackDescription=val.AttackDescription;
+  // var fd=new FormData();
+  // fd.append("AttackTitle",val.AttackTitle);
+  // fd.append("AttackDescription",val.AttackDescription);
+  // return this.http.post('http://127.0.0.1:8000/attackinfo',fd).subscribe();
+  
+  // console.log(val);
 }
 
 
